9ac5575ad15530ac7e3f860beb3c20cb58ee51bd
Aldren Alangcao <aldrenalangcao@Aldren-Alangcao.local>
2015-12-13 15:55:06 +0800

    Added angular UI

--- public/core.js
+++ public/core.js
@@ -1,4 +1,4 @@
-var chowcheckerUI = angular.module('chowcheckerUI', []);
+var chowcheckerUI = angular.module('chowcheckerUI', ['ui.bootstrap.modal', 'ui.bootstrap.tpls']);
 chowcheckerUI.controller('mainController', mainController);
 chowcheckerUI.run(function($http) {
   $http.defaults.headers.common.apiKey = 'Y2hvd2NoZWNrZXIyMDE1';
@@ -9,7 +9,7 @@ chowcheckerUI .config(function ( $httpProvider) {
 
 var baseApiUrl = "https://aqueous-coast-7114.herokuapp.com";
 
-function mainController($scope, $http) {
+function mainController($scope, $http, $uibModal) {
     $scope.ages = ["Puppy", "Adult", "Senior"];
     $scope.results;
 
@@ -33,5 +33,32 @@ function mainController($scope, $http) {
             console.log('Error: ' + data);
         });
     }
+
+    $scope.openModal = function() {
+        $uibModal.open({
+            templateUrl: 'myModalContent.html',
+            controller: 'ModalInstanceCtrl',
+            resolve: {
+                items: function () {
+                  return ['a', 'b', 'c', 'd', 'e'];
+                }
+            }
+        });
+    }
 }
 
+chowcheckerUI.controller('ModalInstanceCtrl', function ($scope, $uibModalInstance, items) {
+
+  $scope.items = items;
+  $scope.selected = {
+    item: $scope.items[0]
+  };
+
+  $scope.ok = function () {
+    $uibModalInstance.close($scope.selected.item);
+  };
+
+  $scope.cancel = function () {
+    $uibModalInstance.dismiss('cancel');
+  };
+});
